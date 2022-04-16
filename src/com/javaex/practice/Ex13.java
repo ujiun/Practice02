cb258510cf05f9820f236f187a235cd6c99b4c77
novacamreader <novacamreader@DESKTOP-SPI7AV4>
2022-04-17 02:06:26 +0900

    2022-04-17 다했다~!

--- src/com/javaex/practice/Ex13.java
+++ src/com/javaex/practice/Ex13.java
@@ -1,10 +1,28 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력해 주세요");
+		System.out.print("숫자: ");
+		double num = sc.nextDouble();
+		
+		double x1 = (num*num*num)-9*num+2;
+		double x2 = (7*num)+2;
+		
+		if (num<=0) {
+			System.out.println("계산결과는 " + x1 + "입니다.");
+		}else {
+			System.out.println("계산결과는 " + x2 + "입니다.");
+		}
+		
+		
+		
+		sc.close();
 	}
 
 }
--- src/com/javaex/practice/Ex14.java
+++ src/com/javaex/practice/Ex14.java
@@ -1,10 +1,41 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("수익을 입력해 주세요");
+		System.out.print("금익: ");
+		
+		double income = sc.nextDouble();
+		
+		double tax1 = 0.09*income;
+		double tax2 = 1000*0.09 + 0.18*(income-1000);
+		double tax3 = 1000*0.09 + 3000*0.18 + 0.27*(income-4000);
+		double tax4 = 1000*0.09 + 3000*0.18 + 4000*0.27 + 0.36*(income-8000);
+		
+		if(0<=income && income<=1000) {
+			System.out.println("소득세는 " + tax1 + "입니다.");
+		}
+		else if(1000<income && income<=4000) {
+			System.out.println("소득세는 " + tax2 + "입니다.");
+		}
+		else if(4000<income && income<8000) {
+			System.out.println("소득세는 " + tax3 + "입니다.");
+		}
+		else if(8000<=income) {
+			System.out.println("소득세는 " + tax4 + "입니다.");
+		}else{
+			System.out.println("잘못 입력했습니다.");
+		}
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
