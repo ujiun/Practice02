03ef15da48b3ba9639bbe21e10a8f034d350c73e
novacamreader <novacamreader@DESKTOP-SPI7AV4>
2022-04-17 04:01:15 +0900

    2022-04-17

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -11,20 +11,13 @@ public class Ex07 {
 		System.out.print("두번째 숫자: ");
 		int num2 = sc.nextInt();
 		
-		int result = num1-num2;
-		int share = num1/num2;
-		int remainder = num1%num2;
 		
-		int share2 = num2/num1;
-		int remainder2 = num2%num1;
-		
-		
-		if(result>0) {
-			System.out.println("목: " + share);
-			System.out.println("나머지: " + remainder);
+		if(num1>num2) {
+			System.out.println("몫: " + num1/num2);
+			System.out.println("나머지: " + num1%num2);
 		}else {
-			System.out.println("목: " + share2);
-			System.out.println("나머지: " + remainder2);
+			System.out.println("몫: " + num2/num1);
+			System.out.println("나머지: " + num2%num1);
 		}
 		
 		
